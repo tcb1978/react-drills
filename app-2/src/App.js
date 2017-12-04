01ff4995503a9c7919e0327516e4bfea9a588ca4
Matthew Eldredge <mattheweldredge@gmail.com>
2017-12-04 16:11:34 -0700

    1st solution for app-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      array: ['spaghetti ', 'ice cream ', 'sushi ', 'bologna ', 'cheese ']
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          <ul>
+            <li>{this.state.array[0]}</li>
+            <li>{this.state.array[1]}</li>
+            <li>{this.state.array[2]}</li>
+            <li>{this.state.array[3]}</li>
+            <li>{this.state.array[4]}</li>
+          </ul>
+        </h1>
       </div>
     );
   }
