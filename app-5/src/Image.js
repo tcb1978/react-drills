dac792008747c3266e7575f9fd57719e4b530b87
Matthew Eldredge <mattheweldredge@gmail.com>
2017-12-04 19:33:55 -0700

    add-5

---
+++ app-5/src/Image.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+
+export const Image = () => {
+    return (
+        <div>
+            <img src="https://http.cat/414" />
+        </div>
+    );
+}
+
+export default Image
